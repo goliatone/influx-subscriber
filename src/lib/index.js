7b7517859ead1666fb322d02f6d84ae138b698f3
goliatone <burgosemi@gmail.com>
2016-08-12 11:34:49 -0400

    Subscriber: listen for messages, parse line protocol, and emit events based on measurement

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
+/*jshint esversion:6, node:true*/
 /*
  * Influx Subscriber
  * https://github.com/goliatone/influx-subscriber
@@ -9,13 +10,15 @@
 
 const degram = require('dgram');
 const extend = require('gextend');
+const parser = require('./parser');
+const Server = require('./server');
 const EventEmitter = require('events');
 
 
-class Subscriber extends EventEmitter{
+class Subscriber extends EventEmitter {
     constructor(config){
         super();
-        config = extend({}, this.constructor.DEFAULTS, config);
+        config = extend({}, Subscriber.DEFAULTS, config);
 
         if(config.autoinitialize) this.init(config);
     }
@@ -24,7 +27,17 @@ class Subscriber extends EventEmitter{
         if(this.initialized) return;
         this.initialized = true;
 
-        extend(this, this.constructor.DEFAULTS, config);
+        this.server = new Server(config.server);
+
+        extend(this, Subscriber.DEFAULTS, config);
+
+        this.server.on('message', (point) => this.handleMessage(point));
+        this.server.start();
+    }
+
+    handleMessage(point){
+        var payload = parser.lineToJSON(point);
+        this.emit(payload.measurement, payload);
     }
 }
 
