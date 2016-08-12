15742ba3e07f926881668aef37765c383c8d8b40
goliatone <burgosemi@gmail.com>
2016-08-12 11:33:45 -0400

    update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+/*jshint esversion:6, node:true*/
+/*
+ * Influx Subscriber
+ * https://github.com/goliatone/influx-subscriber
+ *
+ * Copyright (c) 2016 goliatone
+ * Licensed under the MIT license.
+ */
+'use strict';
+
 /**
  * Exports influx-subscriber main library
  */
-// module.exports = require('./lib');
-module.exports = require('./lib/server');
+module.exports = require('./lib');
